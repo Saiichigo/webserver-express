be049b4737a64f2069cb860df2a45e0b77c6ba92
root <root@Saiichigo.localdomain>
2020-08-15 00:49:44 -0400

    primer commit

---
+++ app-old.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+
+http.createServer((req, res) => {
+
+    res.writeHead(200, {'content-type' : 'application/json'})
+    let salida = {
+        nombre: 'Guillermo',
+        edad: 24,
+        url: req.url
+    }
+    res.write(JSON.stringify( salida ));
+    res.end()
+
+})
+.listen(8080);
+
+console.log('escuchando el puerto 8080')
